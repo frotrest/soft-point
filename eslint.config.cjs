c6393e1b8dc03960121ba9d09400e04500399b56
Frotrest <125584696+frotrest@users.noreply.github.com>
2026-01-11 13:40:02 +0200

    final-fix

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,3 +1,4 @@
+// eslint.config.cjs
 const js = require('@eslint/js');
 const globals = require('globals');
 const prettierConfig = require('eslint-config-prettier');
@@ -15,6 +16,17 @@ module.exports = [
       'no-unused-vars': 'warn',
       'no-console': 'off',
     },
+  },
+  {
+    files: ['tailwind.config.js', 'postcss.config.js', '*.config.js', '*.cjs'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'script',
+      globals: globals.node,
+    },
+    rules: {
+      'no-undef': 'off'
+    }
   },
   prettierConfig,
 ];
